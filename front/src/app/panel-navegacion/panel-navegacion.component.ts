0def08d9469e505f875a852ebe53a510c10d11ca
karolayn Buñay <152365279+Karolayn8756@users.noreply.github.com>
2025-11-12 09:40:05 -0500

    tercerco

--- front/src/app/panel-navegacion/panel-navegacion.component.ts
+++ front/src/app/panel-navegacion/panel-navegacion.component.ts
@@ -1,15 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-panel-navegacion',
   templateUrl: './panel-navegacion.component.html',
   styleUrls: ['./panel-navegacion.component.css']
 })
-export class PanelNavegacionComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
-}
+export class PanelNavegacionComponent {}
